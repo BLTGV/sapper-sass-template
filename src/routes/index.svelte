06f2b2a1723e49d17ef5d470eb74b6e5d5ca1d43
Casey Benko <casey.benko@bltgv.com>
2020-11-18 08:53:22 -0500

    confirmed SCSS working in dev

---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+</script>
+
+<style type="text/scss">
+	$fontsize: 4em;
+
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: $fontsize;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: $fontsize;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
